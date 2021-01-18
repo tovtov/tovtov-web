a36a0c210f7d9c57f983539d96c43c5e5c8c0150
tyannarae <tyannarae1994@gmail.com>
2021-01-18 11:48:58 -0600

    installed bulma

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <button className="button is-primary ">Edit and save to reload.</button>
         <a
           className="App-link"
           href="https://reactjs.org"
