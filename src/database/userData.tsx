9ecba47e3f23848d46cdd5cc57cd385f90ac8dd2
tyannarae <tyannarae1994@gmail.com>
2021-01-21 14:01:19 -0600

    still working on the interface for the kudos component

--- src/database/userData.tsx
+++ src/database/userData.tsx
@@ -1,4 +1,4 @@
-export interface kudo {
+export interface Kudo {
   giverImg: string;
   giversName: string;
   giversUserName: string;
@@ -9,10 +9,31 @@ export interface User {
   userImg: string;
   userName: string;
   fullName: string;
-  kudos: Array<kudo>;
+  kudos: Kudo[];
 }
 
 export const UserData: Array<User> = [
+  {
+    userImg: "https://bulma.io/images/placeholders/128x128.png",
+    userName: "@testName",
+    fullName: "test name",
+    kudos: [
+      {
+        giverImg: "https://bulma.io/images/placeholders/128x128.png",
+        giversName: "giver",
+        giversUserName: "@giver",
+        kudo: "heres some props!",
+        date: new Date(),
+      },
+      {
+        giverImg: "https://bulma.io/images/placeholders/128x128.png",
+        giversName: "giver2",
+        giversUserName: "@giver2",
+        kudo: "making things happen!",
+        date: new Date(),
+      },
+    ],
+  },
   {
     userImg: "https://bulma.io/images/placeholders/128x128.png",
     userName: "@testName",
--- src/pages/pageProfile.tsx
+++ src/pages/pageProfile.tsx
@@ -7,27 +7,20 @@ import "./pageProfile.scss";
 export interface pageProfileProps {
   user?: User[]; //the users chosen plants interface
 }
-const PageProfile: FunctionComponent<pageProfileProps> = (
-  kudos,
-  userImg: User
-) => {
-  console.log(kudos, userImg);
+const PageProfile: FunctionComponent<pageProfileProps> = (kudos, user) => {
+  console.log(kudos, user);
   return (
     <div>
       <ProfileHeader />
       <section className=" section ">
-        <div className="notification is-light">
-          <p className="title is-4 is-align-items-flex-end kudosTitle">Kudos</p>
-          <Kudo />
-        </div>
-
-        {/* {UserData.map((userPlant: UserPlant, index: number) => (
-          <CollectionBox
-            key={index}
-            userPlant={userPlant}
-            userPlantindex={index as number}
-          />
-        ))} */}
+        {UserData.map((user: User, index: number) => (
+          <div className="notification is-light">
+            <p className="title is-4 is-align-items-flex-end kudosTitle">
+              Kudos
+            </p>
+            <Kudo />
+          </div>
+        ))}
       </section>
     </div>
   );
