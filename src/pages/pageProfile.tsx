e4bf6d9d034736ad2bfcc3b4126b9c7ece371eba
tyannarae <tyannarae1994@gmail.com>
2021-01-21 14:24:28 -0600

    comitting before I screw things up

--- src/pages/pageProfile.tsx
+++ src/pages/pageProfile.tsx
@@ -1,26 +1,27 @@
 import React, { FunctionComponent } from "react";
 import ProfileHeader from "../components/profileHeader";
 import Kudo from "../components/kudo";
-import { User, UserData } from "../database/userData";
+import { User, UserData, SingleKudo } from "../database/userData";
 import "./pageProfile.scss";
 
 export interface pageProfileProps {
-  user?: User[]; //the users chosen plants interface
+  user?: User; //the users chosen plants interface
 }
 const PageProfile: FunctionComponent<pageProfileProps> = (kudos, user) => {
-  console.log(kudos, user);
   return (
     <div>
       <ProfileHeader />
       <section className=" section ">
-        {UserData.map((user: User, index: number) => (
-          <div className="notification is-light">
-            <p className="title is-4 is-align-items-flex-end kudosTitle">
-              Kudos
-            </p>
-            <Kudo />
-          </div>
-        ))}
+        {UserData.map((user: User, index: number) =>
+          user.kudos.map((x: SingleKudo, i: number) => (
+            <div className="notification is-light">
+              <p className="title is-4 is-align-items-flex-end kudosTitle">
+                Kudos
+              </p>
+              <Kudo />
+            </div>
+          ))
+        )}
       </section>
     </div>
   );
