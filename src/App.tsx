714785d4f6b99b5e9341783d24003ec7359f30b6
tyannarae <tyannarae1994@gmail.com>
2021-03-09 14:46:02 -0600

    getting ready to deploy to github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import ProfilePage from "./pages/profilePage/profilePage";
 import "./App.scss";
 
 const App = () => {
   return (
-    <div className="App">
-      <ProfilePage />
-    </div>
+    <Router>
+      <div className="App">
+        <Route exact path="/tovtov" component={ProfilePage}></Route>
+      </div>
+    </Router>
   );
 };
 
